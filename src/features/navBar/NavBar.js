d5b2b284394352da9262e16297e52760d001cefc
kairos4213 <jamesdkairos@gmail.com>
2023-08-17 17:12:45 -0500

    Initialize Navbar w/basic search input

---
+++ src/features/navBar/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div>
+            <h1>/R</h1>
+            <input placeholder="Search here!"></input>
+        </div>
+    )
+}
